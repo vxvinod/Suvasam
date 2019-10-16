c957888caca8745f9ca6362926a015e4909d3e41
vxvinod <vinodkmrmurugan@gmail.com>
2019-10-16 14:15:24 +0530

    map added to donate fragment

--- app/src/main/java/com/example/suvasam/DonateFragment.java
+++ app/src/main/java/com/example/suvasam/DonateFragment.java
@@ -5,10 +5,20 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
@@ -19,12 +29,12 @@ import android.view.ViewGroup;
  * Use the {@link DonateFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DonateFragment extends Fragment {
+public class DonateFragment extends Fragment implements OnMapReadyCallback {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private GoogleMap map;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -60,13 +70,25 @@ public class DonateFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donate, container, false);
+        View view =  inflater.inflate(R.layout.fragment_donate, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if(mapFragment == null) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -91,6 +113,33 @@ public class DonateFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng madurai = new LatLng(9.922939, 78.114941);
+
+        LatLng maduraiLeft = new LatLng(9.924006, 78.064849);
+        LatLng maduraiRight = new LatLng(9.929358, 78.219317);
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(maduraiLeft);
+        builder.include(maduraiRight);
+
+        LatLngBounds bounds = builder.build();
+
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+
+        int padding = (int) (width * 0.20);
+
+        map.setLatLngBoundsForCameraTarget(bounds);
+
+        map.addMarker(new MarkerOptions().position(madurai).title("Madurai"));
+        map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding));
+
+        map.setMinZoomPreference(map.getCameraPosition().zoom);
     }
 
     /**
