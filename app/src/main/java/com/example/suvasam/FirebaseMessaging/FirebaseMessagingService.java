4813a435c6188ea3e18ed9d81d65e2ae3dc50550
vxvinod <vinodkmrmurugan@gmail.com>
2019-10-18 21:13:37 +0530

    firebase cloud notification done

---
+++ app/src/main/java/com/example/suvasam/FirebaseMessaging/FirebaseMessagingService.java
@@ -0,0 +1,32 @@
+package com.example.suvasam.FirebaseMessaging;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+
+import com.example.suvasam.R;
+import com.google.firebase.messaging.RemoteMessage;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+}
